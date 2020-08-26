1a7cfb0fa0d48ab271b33fd3ac8700a27847b6a4
dante12357 <dantera16@gmail.com>
2020-08-26 11:42:20 +0300

    Updates

---
+++ precache-manifest.577c065371ccc5439cb4e9b96994fd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d046049d39413f677f7d4bcf437c1b",
+    "url": "/ozi/index.html"
+  },
+  {
+    "revision": "760cc665b67b24e8ce8f",
+    "url": "/ozi/static/css/main.7d04a317.chunk.css"
+  },
+  {
+    "revision": "fb80a30b4afd49aa7131",
+    "url": "/ozi/static/js/2.c21bdad5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ozi/static/js/2.c21bdad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760cc665b67b24e8ce8f",
+    "url": "/ozi/static/js/main.4011ae20.chunk.js"
+  },
+  {
+    "revision": "45c9afa941f565125122",
+    "url": "/ozi/static/js/runtime-main.c9fd7a30.js"
+  }
+]);
